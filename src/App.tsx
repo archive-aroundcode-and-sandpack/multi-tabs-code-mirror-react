404901e3e0c04b4f14b585a74891bee42a963788
Jocelin Queau <queau.j@gmail.com>
2023-12-26 17:16:16 +0100

    bugged setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { useState } from 'react'
+import { Editor, languageExtensionMap } from './Editor'
 
 type CodeFiles =  Record<string, { code: string, active?: boolean }>
 const startingFiles: CodeFiles = {
   'index.js': {
-    code: `console.log('Hello World')`
+    code: `console.log('Hello World')`,
+    active: true
   },
   'index.html': {
     code: `<h1>Hello World</h1>`
@@ -14,13 +16,48 @@ const startingFiles: CodeFiles = {
 }
 
 function App() {
-  const [files, setFiles] = useState<CodeFiles>(startingFiles)
-  const activeFile = Object.keys(files).find((file) => files[file].active) || ''
-  const code = files[activeFile].code
-  const languageExtension = activeFile.split('.').pop() || ''
+  const [files, setFiles] = useState<CodeFiles>(startingFiles);
+  const activeFile = Object.keys(files).find((file) => files[file].active) || '';
+  const code = files[activeFile].code;
+  const languageExtension = activeFile.split('.').pop() as keyof typeof languageExtensionMap;
+
+  const handleCodeUpdate = (code: string) => {
+    setFiles((files) => ({
+      ...files,
+      [activeFile]: {
+        code
+      }
+    }))
+  }
+
+  const updateActiveFile = (filename: string) => {
+    setFiles((files) => ({
+      ...files,
+      [activeFile]: {
+        ...files[activeFile],
+        active: false
+      },
+      [filename]: {
+        ...files[filename],
+        active: true
+      }
+    }))
+  }
 
   return (
     <>
+      <div data-is="tabs">
+      {Object.keys(files).map((filename) => (
+        <button key={filename} onClick={() => updateActiveFile(filename)}>
+          {filename}
+        </button>
+      ))}
+      </div>
+      <Editor 
+        code={code} 
+        extension={languageExtension} 
+        onCodeUpdate={handleCodeUpdate}  
+      />
     </>
   )
 }
--- src/Editor.tsx
+++ src/Editor.tsx
@@ -1,8 +1,93 @@
+import { useEffect, useRef, useState } from "react";
+
+import {
+  EditorState
+} from '@codemirror/state'
+
+import {
+  EditorView
+} from '@codemirror/view'
+
+import { javascript } from "@codemirror/lang-javascript";
+import { css } from "@codemirror/lang-css";
+import { html } from "@codemirror/lang-html";
+import { basicSetup } from "codemirror";
+
 // the editor only deal with the code
 interface EditorProps {
   code: string;
-  extension: string;
+  extension: keyof typeof languageExtensionMap;
   onCodeUpdate: (code: string) => void;
 }
 
-export const Editor = () => {}
+export const languageExtensionMap = {
+  'tsx': 'typescript',
+  'ts': 'typescript',
+  'js': 'javascript',
+  'jsx': 'javascript',
+  'html': 'html',
+  'css': 'css'
+} as const
+
+export const Editor = ({ code, extension, onCodeUpdate }: EditorProps) => {
+  const [internalCode, setInternalCode] = useState(code);
+  const wrapper = useRef<HTMLDivElement>(null);
+  const cmView = useRef<EditorView | null>(null);
+
+  useEffect(() => { 
+    const languagueExtension = languageExtensionMap[extension];
+
+    const languagesSupport = {
+      javascript: javascript({ jsx: true, typescript: false }),
+      typescript: javascript({ jsx: true, typescript: true }),
+      html: html(),
+      css: css(),
+    }
+
+    const editorState = EditorState.create({
+      doc: code,
+      extensions:[
+        basicSetup,
+        languagesSupport[languagueExtension]
+      ]
+    });
+
+    // you could also use a ref here
+    const view = new EditorView({
+      state: editorState,
+      parent: document.querySelector('#editor')!,
+      dispatch: (tr) => {
+        view.update([tr]);
+
+        if (tr.docChanged) {
+          const newCode = tr.newDoc.sliceString(0, tr.newDoc.length);
+          setInternalCode(newCode);
+          onCodeUpdate(newCode);
+        }
+      },
+    });
+
+    cmView.current = view;
+
+    return () => {
+      view.destroy();
+    };
+  }, [])
+
+  useEffect(() => {
+    // this only run when updating the active file
+    const isSameCode = code === internalCode;
+    if (cmView.current && !isSameCode) {
+      const view = cmView.current;
+      view.dispatch({
+        changes: { from: 0, to: view.state.doc.length, insert: code },
+      });
+    }
+  }, [code])
+
+  return (
+    <div ref={wrapper}>
+      <div id="editor"></div>
+    </div>
+  )
+}
